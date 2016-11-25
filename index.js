ba8d3a87a56c81745b758795967bad25d7c7e5c6
nbkhope <ryumemaru@yahoo.com>
2016-11-25 15:20:21 -0800

    Add return statement after console log when error occurs in exec()

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const command = `json-server --watch db.json --port ${port}`;
 exec(command, (err, stdout, stderr) => {
   if (err) {
     console.log('Error running exec', err);
+    return;
   }
   console.log('stdout:', stdout);
   console.log('stderr:', stderr);
