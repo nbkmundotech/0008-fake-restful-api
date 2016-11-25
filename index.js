ca9dd3f4cae64d7c7d4840281c4601faaa74ce36
nbkhope <ryumemaru@yahoo.com>
2016-11-25 15:16:43 -0800

    Add index.js to fire up json-server from Node script

---
+++ index.js
@@ -0,0 +1,15 @@
+const { exec } = require('child_process');
+
+// Set port (default: 3000). For Heroku, we need to use
+// the port set by the environment variable $PORT
+const port = process.env.PORT || 3000;
+
+const command = `json-server --watch db.json --port ${port}`;
+
+exec(command, (err, stdout, stderr) => {
+  if (err) {
+    console.log('Error running exec', err);
+  }
+  console.log('stdout:', stdout);
+  console.log('stderr:', stderr);
+});
